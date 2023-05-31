26817220255147518e29fd69ac9d6af42e7465c9
skrmetche <84297060+skrmetche@users.noreply.github.com>
2023-05-31 17:07:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,15 @@ def main():
         file_contents = file.read()
         st.write("File contents:")
         st.write(file_contents)
+        
+        import streamlit as st
+
+def main1():
+    st.title("Align Text Example")
+    st.write("<p style='text-align: center;'>Center aligned text</p>", unsafe_allow_html=True)
+
 
 if __name__ == "__main__":
     main()
+    main1()
     st.write('Hello, *World!* :sunglasses:')
